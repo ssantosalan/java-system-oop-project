341bf6240ca2ac4fdfd349e94c3f910a86cba6c6
Alan Santos <ss.alan@outlook.com>
2022-04-24 14:17:58 -0300

    Commit from Eclipse

--- Projeto/src/main/java/com/example/fxproject/HelloApplication.java
+++ Projeto/src/main/java/com/example/fxproject/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Desenvolvendo na base do ódio!");
+        stage.setTitle("Desenvolvendo na base do ódio! Eclipse or IntelliJ");
         stage.setScene(scene);
         stage.show();
     }
