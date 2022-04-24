749dc817a63567923c01d94da8986787f816e4c7
Alan Santos <ss.alan@outlook.com>
2022-04-24 14:55:25 -0300

    Commit from vsCode 02

--- Projeto/src/main/java/com/example/fxproject/HelloController.java
+++ Projeto/src/main/java/com/example/fxproject/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("WELCOME, DEVELOPER! IT'S A NEW ERA!" );
     }
 }
