7532e2a9231da306e3285b2cab83e58f2b5801e7
Alan Santos <ss.alan@outlook.com>
2022-05-02 11:51:47 -0300

    Rename do main, controller e arquivo fxml. Criação de variáveis na classe Vendedor

--- Projeto/src/main/java/pessoas/Vendedor.java
+++ Projeto/src/main/java/pessoas/Vendedor.java
@@ -1,4 +1,9 @@
 package pessoas;
 
-public class Vendedor {
+public class Vendedor extends Pessoa {
+    private int idVendedor;
+    private String nomeVendedor;
+    private String emailVendedor;
+    private String senhaVendedor;
+    private double salarioVendedor;
 }
