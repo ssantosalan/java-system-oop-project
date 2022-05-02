b0a3bd4bf5d759c432c4a646d5ecccfc2774cdbf
Alan Santos <ss.alan@outlook.com>
2022-05-02 16:12:01 -0300

    Criado um controlador de telas.

---
+++ Projeto/src/main/java/com/example/fxproject/SceneController.java
@@ -0,0 +1,33 @@
+package com.example.fxproject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void trocarParaTelaMain(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void trocarParaTelaVendedor(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("tela-vendedor.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+}
--- Projeto/src/main/java/pessoas/Vendedor.java
+++ Projeto/src/main/java/pessoas/Vendedor.java
@@ -1,4 +1,6 @@
 package pessoas;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class Vendedor extends Pessoa {
     private int idVendedor;
@@ -6,4 +8,55 @@ public class Vendedor extends Pessoa {
     private String emailVendedor;
     private String senhaVendedor;
     private double salarioVendedor;
+
+    @FXML
+    private Button botaoVendedor;
+
+    public Vendedor(int idVendedor, String nomeVendedor, String emailVendedor, String senhaVendedor, double salarioVendedor) {
+        this.idVendedor = idVendedor;
+        this.nomeVendedor = nomeVendedor;
+        this.emailVendedor = emailVendedor;
+        this.senhaVendedor = senhaVendedor;
+        this.salarioVendedor = salarioVendedor;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public String getNomeVendedor() {
+        return nomeVendedor;
+    }
+
+    public void setNomeVendedor(String nomeVendedor) {
+        this.nomeVendedor = nomeVendedor;
+    }
+
+    public String getEmailVendedor() {
+        return emailVendedor;
+    }
+
+    public void setEmailVendedor(String emailVendedor) {
+        this.emailVendedor = emailVendedor;
+    }
+
+    public String getSenhaVendedor() {
+        return senhaVendedor;
+    }
+
+    public void setSenhaVendedor(String senhaVendedor) {
+        this.senhaVendedor = senhaVendedor;
+    }
+
+    public double getSalarioVendedor() {
+        return salarioVendedor;
+    }
+
+    public void setSalarioVendedor(double salarioVendedor) {
+        this.salarioVendedor = salarioVendedor;
+    }
 }
