baaabf5a789983f90762a5e83cced6973ebc30d5
Alan Santos <ss.alan@outlook.com>
2022-05-04 13:15:57 -0300

    Variáveis, Construtor e Getters and Setters da classe Pessoa

--- Projeto/src/main/java/pessoas/Pessoa.java
+++ Projeto/src/main/java/pessoas/Pessoa.java
@@ -1,4 +1,37 @@
 package pessoas;
 
 public class Pessoa {
+    private String senhaPessoa;
+    private String emailPessoa;
+    private int id;
+
+    public Pessoa(String senhaPessoa, String emailPessoa, int id) {
+        this.senhaPessoa = senhaPessoa;
+        this.emailPessoa = emailPessoa;
+        this.id = id;
+    }
+
+    public String getSenhaPessoa() {
+        return senhaPessoa;
+    }
+
+    public void setSenhaPessoa(String senhaPessoa) {
+        this.senhaPessoa = senhaPessoa;
+    }
+
+    public String getEmailPessoa() {
+        return emailPessoa;
+    }
+
+    public void setEmailPessoa(String emailPessoa) {
+        this.emailPessoa = emailPessoa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
