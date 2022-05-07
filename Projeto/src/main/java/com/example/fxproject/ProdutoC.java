5f1d2e4f69fc390222fe114d9ea13508f1554d87
Jorge Oliveira <jorgelsol.1902@gmail.com>
2022-05-06 21:34:36 -0300

    Adicionado a classe ProdutoC, onde estão os getters e setters do produto

---
+++ Projeto/src/main/java/com/example/fxproject/ProdutoC.java
@@ -0,0 +1,52 @@
+package com.example.fxproject;
+
+public class ProdutoC {
+
+    String descricao;
+    double valor;
+    int id, quantidade;
+
+    public ProdutoC() {
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void pesquisarId (int id){
+
+    }
+
+    public void pesquisarDescricao ( String descricao){
+
+    }
+}
